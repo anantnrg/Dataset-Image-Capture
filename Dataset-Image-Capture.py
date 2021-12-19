57101a7edec4d19bc09bb26f32a59a733e65f24c
Anant Narayan <47025756+anantnrg@users.noreply.github.com>
2021-12-20 00:07:53 +0530

    Add files via upload

---
+++ Dataset-Image-Capture.py
@@ -0,0 +1,40 @@
+# Imports
+import cv2
+import argparse
+
+# Parse Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--object', type=str, required=True)
+args = parser.parse_args()
+
+# Intialize OpenCV image capture
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("Dataset Image Capture")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("OpenCV couldn't capture the image, please try again!")
+        break
+    cv2.imshow("Dataset Image Capture", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        conf = input('Are you sure you wanna quit? Y - Yes, N - No : ')
+        if conf == 'y' or 'Y':
+            print("Closing Dataset Image Capture....")
+            break
+        else:
+            pass
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "{}_{}.png".format(args.object, img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} captured!".format(img_name))
+        img_counter += 1
+
+cam.release()
+cv2.destroyAllWindows()
